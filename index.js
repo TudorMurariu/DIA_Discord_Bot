f68819d0b132cbb6f40aac948a0a46f1e31d1493
Tudor <tudorcristianmurariu@yahoo.com>
2022-09-27 23:21:53 +0300

    added member count function

--- index.js
+++ index.js
@@ -20,20 +20,36 @@ let TOKEN = prompt("Enter TOKEN : ");
 const client = new Discord.Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGES,
     ]
 });
 
 client.on('ready', () => {
     console.log("YAY");
-    client.user.setActivity('mama ta', { type: 'WATCHING' });
-    try {
-        client.user.setAvatar('./emil.png');
-        client.user.setUsername('DIA Discord Bot');
-    } catch (e) {
-        // we do nottin abt it
-    }
+    //client.user.setActivity('mama ta', { type: 'WATCHING' });
+    // try {
+    //     client.user.setAvatar('./emil.png');
+    //     client.user.setUsername('DIA Discord Bot');
+    // } catch (e) {
+    // we do nottin abt it
+    //}
+
+    // checks once every 3 secconds if you are playing LOL
+    // async function check_LOL() {
+
+
+
+    //     await Sleep(3000); // we create a delay
+    //     await check_LOL(); // we call the function again
+    // }
+
+    // check_LOL();
 });
+
+
+
+
 
 const days = ["duminica", "luni", "marti", "miercuri my dudes", "joi", "vineri", "sambata"];
 
@@ -52,11 +68,55 @@ client.on("messageCreate", (message) => {
         switch (args[1]) {
             case "meow":
                 break;
-            case "become_me":
-                //client.user.setUsername(message.author.username);
-                message.guild.
-                console.log(message.author.username);
-                client.user.setAvatar(message.author.avatarURL());
+                // case "become_me":
+                //     //client.user.setUsername(message.author.username);
+                //     message.guild.
+                //     console.log(message.author.username);
+                //     client.user.setAvatar(message.author.avatarURL());
+                //     break;
+
+            case "getServerID":
+                {
+                    let server_id = message.guild.id;
+                    message.reply(server_id + " ");
+                }
+                break;
+
+            case "showUserCount":
+                message.reply(String(message.guild.memberCount));
+                break;
+
+            case "showAllUsers":
+                {
+                    // const server_id = message.guild.id;
+                    // const guild = client.guilds.resolve(server_id);
+                    // let res = "";
+
+                    //guild.members.fetch({ force: true })
+                    //.then(console.log)
+                    //.catch(console.error);
+
+                    //guild.members.fetch()
+                    //.then(console.log("member"))
+                    //.catch(console.error("meow"));
+
+                    message.guild.members.fetch()
+                    .then(members => {
+                        members.forEach(member => console.log(member.nickname));
+                        console.log("hi");
+                    });
+
+                    message.guild.members.fetch().then(members => {
+                        // Loop through every members
+                        console.log("hi");
+                        members.forEach(member => {
+                            // Do whatever you want with the current member
+                            console.log(member.nickname);
+                        });
+                    });
+
+                    message.react("🤡");
+                }
                 break;
         }
     }
